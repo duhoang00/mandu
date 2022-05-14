a7fae94b2f7e7a5aa7b4ac8f4b3fe4227b7e6f3f
Du Hoang <hoangminhdu2000@gmail.com>
2022-05-14 15:05:14 +0700

    using graphql

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_14_075641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "product_types", force: :cascade do |t|
+    t.string "product_type_color"
+    t.string "product_type_size"
+    t.string "product_type_image_url"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_types_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "product_brand"
+    t.string "product_name"
+    t.string "product_description"
+    t.string "product_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "product_types", "products"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+chocopiepie = Product.create!(
+  product_brand: "Choco",
+  product_name: "Chocopie",
+  product_description: "Yummy!",
+  product_image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/LOTTE_-_CHOCO_PIE.jpg/1280px-LOTTE_-_CHOCO_PIE.jpg",
+)
+
+ProductType.create!([
+  {
+    product_type_color: "Red",
+    product_type_size: "S",
+    product_type_image_url: "",
+    product: chocopiepie,
+  },
+  {
+    product_type_color: "Green",
+    product_type_size: "M",
+    product_type_image_url: "",
+    product: chocopiepie,
+  },
+  {
+    product_type_color: "Blue",
+    product_type_size: "L",
+    product_type_image_url: "",
+    product: chocopiepie,
+  },
+])
