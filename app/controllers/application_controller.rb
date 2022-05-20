bca63c04d897cbb29eee2f0d5bd25820089d0791
Du Hoang <hoangminhdu2000@gmail.com>
2022-05-20 12:40:33 +0700

    overwrite host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def default_url_options
+    if Rails.env.production?
+      { :host => "https://mandu-app.herokuapp.com/" }
+    else
+      {}
+    end
+  end
 end
